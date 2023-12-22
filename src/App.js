ca2504758c3a33b14df84b503b1e582f56dde12b
Govind <govinddevtech139@gmail.com>
2023-12-22 12:59:24 +0530

    apis changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import styled from '@emotion/styled';
 import './App.css';
+import  Form  from './components/Form';
+import { Typography } from '@mui/material';
+
 
 function App() {
+  const StickyHeader = styled("div")({
+    position: "sticky",
+    top: 0,
+    zIndex: 100,
+    backgroundColor: "blue", 
+    background: "linear-gradient(45deg, #f54b4b 30%, #a7a7a7 90%)",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    borderRadius: "0px 0px 10px 10px",
+    color:"white",
+
+    padding: (theme) => theme.spacing(2),
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <StickyHeader>
+        <Typography variant="h3" gutterBottom >
+          MERN Stack App
+        </Typography>
+      </StickyHeader>
+    <Form/>
     </div>
   );
 }
 
 export default App;
+
